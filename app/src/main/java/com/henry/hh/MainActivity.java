e2b47cecafd5b2818917a433f30c68e496b99c91
Henry-Mark <heneymark@gmail.com>
2016-09-24 22:25:43 +0800

    project

--- app/src/main/java/com/henry/hh/MainActivity.java
+++ app/src/main/java/com/henry/hh/MainActivity.java
@@ -2,6 +2,9 @@ package com.henry.hh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.henry.hh.utils.ToastUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToastUtils.show(this,"Hello",5000);
     }
 
     @Override
---
+++ commonLibrary/src/main/java/com/henry/hh/Constants/Constant.java
@@ -0,0 +1,15 @@
+package com.henry.hh.Constants;
+
+/**
+ * Date: 16-9-24 下午10:13
+ * Creator: henry
+ * Email: heneymark@gmail.com
+ * Description:常量
+ */
+public class Constant {
+    /**
+     * sharedpreference 保存文件名
+     */
+    public static final String FILENAME_SP = "imu_data";
+
+}
