5cc6d2f18195029261f07a90046558e383be38c3
Henry <heneymark@gmail.com>
2016-09-27 11:17:45 +0800

    IMU

--- app/src/main/java/com/henry/hh/activity/MainActivity.java
+++ app/src/main/java/com/henry/hh/activity/MainActivity.java
@@ -2,8 +2,6 @@ package com.henry.hh.activity;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -34,6 +32,8 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         bindControls();
 
+
+
     }
 
     /**
@@ -42,8 +42,9 @@ public class MainActivity extends BaseActivity {
     protected void bindControls() {
         drawer_layout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawer = (LinearLayout) findViewById(R.id.ll_drawer);
+        //设置抽屉宽度为频幕宽度的3/5
+        ControlsUtils.setWidth(mDrawer, ScreenUtils.getScreenWidth(this) * 3 / 5);
         mText = (TextView) findViewById(R.id.text1);
-
     }
 
 
