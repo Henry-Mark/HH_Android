5210d0786ed1bff98743b9cfe5843ce6d7449a12
Henry <heneymark@gmail.com>
2016-09-27 17:26:18 +0800

    IMU

--- app/src/main/java/com/henry/hh/activity/GuideActivity.java
+++ app/src/main/java/com/henry/hh/activity/GuideActivity.java
@@ -1,8 +1,20 @@
 package com.henry.hh.activity;
 
+import android.os.Bundle;
+
+import com.henry.hh.R;
 import com.henry.library.activity.BaseActivity;
 
+/**
+ * app引导页面
+ */
 public class GuideActivity extends BaseActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
 
+        startActivity(MainActivity.class);
+    }
 }
