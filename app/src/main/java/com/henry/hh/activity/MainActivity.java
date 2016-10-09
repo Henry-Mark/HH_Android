972b5a09c44cd6397e2979db01a1ff03c0f70f5f
Henry <heneymark@gmail.com>
2016-10-09 17:28:33 +0800

    tab

--- app/src/main/java/com/henry/hh/activity/MainActivity.java
+++ app/src/main/java/com/henry/hh/activity/MainActivity.java
@@ -1,9 +1,7 @@
 package com.henry.hh.activity;
 
-import android.graphics.Color;
-import android.support.v4.app.FragmentTabHost;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,38 +11,43 @@ import android.widget.TextView;
 
 import com.henry.hh.R;
 import com.henry.hh.constants.TabDatas;
+import com.henry.library.activity.BaseActivity;
 
-public class MainActivity extends AppCompatActivity implements TabHost.OnTabChangeListener {
+public class MainActivity extends BaseActivity implements TabHost.OnTabChangeListener {
     private FragmentTabHost tabHost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tabHost=(FragmentTabHost)super.findViewById(android.R.id.tabhost);
-        tabHost.setup(this,super.getSupportFragmentManager()
-                ,R.id.contentLayout);
+        tabHost = (FragmentTabHost) super.findViewById(android.R.id.tabhost);
+        tabHost.setup(this, super.getSupportFragmentManager()
+                , R.id.contentLayout);
         tabHost.getTabWidget().setDividerDrawable(null);
         tabHost.setOnTabChangedListener(this);
         initTab();
 
     }
 
-    private void initTab(){
-        String tabs[]=TabDatas.getTabsTxt();
-        for(int i=0;i<tabs.length;i++){
-            TabHost.TabSpec tabSpec=tabHost.newTabSpec(tabs[i]).setIndicator(getTabView(i));
-            tabHost.addTab(tabSpec,TabDatas.getFragments()[i],null);
+    private void initTab() {
+        String tabs[] = TabDatas.getTabsTxt();
+        for (int i = 0; i < tabs.length; i++) {
+            TabHost.TabSpec tabSpec = tabHost.newTabSpec(tabs[i]).setIndicator(getTabView(i));
+            tabHost.addTab(tabSpec, TabDatas.getFragments()[i], null);
             tabHost.setTag(i);
         }
     }
-    private View getTabView(int idx){
-        View view= LayoutInflater.from(this).inflate(R.layout.footer_tabs,null);
-        ((TextView)view.findViewById(R.id.title_footer)).setText(TabDatas.getTabsTxt()[idx]);
-        if(idx==0){
-            ((TextView)view.findViewById(R.id.title_footer)).setTextColor(Color.RED);
-            ((ImageView)view.findViewById(R.id.img_footer)).setImageResource(TabDatas.getTabsImgLight()[idx]);
-        }else{
-            ((ImageView)view.findViewById(R.id.img_footer)).setImageResource(TabDatas.getTabsImg()[idx]);
+
+    private View getTabView(int idx) {
+        View view = LayoutInflater.from(this).inflate(R.layout.footer_tabs, null);
+        ((TextView) view.findViewById(R.id.title_footer)).setText(TabDatas.getTabsTxt()[idx]);
+        if (idx == 0) {
+
+            ((TextView) view.findViewById(R.id.title_footer)).setTextColor(getResources().getColor(R.color.colorMainStyle));
+
+            ((ImageView) view.findViewById(R.id.img_footer)).setImageResource(TabDatas.getTabsImgLight()[idx]);
+        } else {
+            ((ImageView) view.findViewById(R.id.img_footer)).setImageResource(TabDatas.getTabsImg()[idx]);
         }
         return view;
     }
@@ -54,15 +57,16 @@ public class MainActivity extends AppCompatActivity implements TabHost.OnTabChan
         updateTab();
     }
 
-    private void updateTab(){
-        TabWidget tabw=tabHost.getTabWidget();
-        for(int i=0;i<tabw.getChildCount();i++){
-            View view=tabw.getChildAt(i);
-            ImageView iv=(ImageView)view.findViewById(R.id.img_footer);
-            if(i==tabHost.getCurrentTab()){
-                ((TextView)view.findViewById(R.id.title_footer)).setTextColor(Color.RED);
+    private void updateTab() {
+        TabWidget tabw = tabHost.getTabWidget();
+        for (int i = 0; i < tabw.getChildCount(); i++) {
+            View view = tabw.getChildAt(i);
+            ImageView iv = (ImageView) view.findViewById(R.id.img_footer);
+            if (i == tabHost.getCurrentTab()) {
+                ((TextView) view.findViewById(R.id.title_footer)).setTextColor(getResources().getColor(R.color.colorMainStyle));
                 iv.setImageResource(TabDatas.getTabsImgLight()[i]);
-            }else{        ((TextView)view.findViewById(R.id.title_footer)).setTextColor(Color.GRAY);
+            } else {
+                ((TextView) view.findViewById(R.id.title_footer)).setTextColor(getResources().getColor(R.color.colorTabTextNormal));
                 iv.setImageResource(TabDatas.getTabsImg()[i]);
             }
 
