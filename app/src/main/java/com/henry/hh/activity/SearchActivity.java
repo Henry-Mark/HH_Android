67f8c833778c3c52999eb1a9422e8229a73e6ed2
Henry <heneymark@gmail.com>
2016-10-27 11:19:49 +0800

    IMU

--- app/src/main/java/com/henry/hh/activity/SearchActivity.java
+++ app/src/main/java/com/henry/hh/activity/SearchActivity.java
@@ -1,6 +1,8 @@
 package com.henry.hh.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -10,27 +12,41 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.henry.hh.R;
+import com.henry.hh.adapter.SearchAdapter;
+import com.henry.hh.entity.Friend;
+import com.henry.hh.interfaces.OnRecyclerItemClickListener;
+import com.henry.library.View.DividerItemDecoration;
 import com.henry.library.activity.BaseActivity;
 import com.henry.library.utils.ControlsUtils;
 import com.henry.library.utils.ScreenUtils;
 import com.henry.library.utils.ToastUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchActivity extends BaseActivity
-        implements View.OnClickListener, TextWatcher, View.OnKeyListener {
+        implements View.OnClickListener, TextWatcher, View.OnKeyListener,
+        OnRecyclerItemClickListener {
 
     private ImageView mBack;
     private EditText mSearch;
     private ImageView mSearchImg;
     private LinearLayout mLayoutSearch;
     private ImageView mClear;
+    private TextView mListTitle;
+    private RecyclerView mRecyclerView;
+    private SearchAdapter searchAdapter;
+    private LinearLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         bindView();
+        initList();
     }
 
     /**
@@ -42,19 +58,63 @@ public class SearchActivity extends BaseActivity
         mSearch = getViewById(R.id.et_search);
         mSearchImg = getViewById(R.id.iv_search);
         mClear = getViewById(R.id.iv_clear);
+        mListTitle = getViewById(R.id.tv_list_title);
+        mRecyclerView = getViewById(R.id.recycler_search);
         mSearchImg.setOnClickListener(this);
         mBack.setOnClickListener(this);
         mSearch.addTextChangedListener(this);
         mSearch.setOnKeyListener(this);
         mClear.setOnClickListener(this);
-        ControlsUtils.setWidth(mLayoutSearch, ScreenUtils.getScreenWidth(mContext) * 4 / 5);
+        ControlsUtils.setWidth(mLayoutSearch, ScreenUtils.getScreenWidth(mContext) * 7 / 10);
+    }
+
+    /**
+     * 初始化列表
+     */
+    private void initList() {
+        //创建默认的线性LayoutManager
+        mLayoutManager = new LinearLayoutManager(mContext);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        //如果可以确定每个item的高度是固定的，设置这个选项可以提高性能
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(mContext,
+                DividerItemDecoration.VERTICAL_LIST));
+        //创建并设置Adapter
+        searchAdapter = new SearchAdapter(mContext);
+        mRecyclerView.setAdapter(searchAdapter);
+        searchAdapter.addOnItemClickListener(this);
+        searchHistory();
+    }
+
+    /**
+     * 搜索历史
+     */
+    private void searchHistory() {
+        mListTitle.setText(getString(R.string.history_of_search));
+        searchAdapter.setSearched(false);
+        searchAdapter.refresh(getDatas(1));
     }
 
     /**
      * 搜索
      */
     private void search() {
-        ToastUtils.showShort(mContext, "search");
+        mListTitle.setText(getString(R.string.string_contacts));
+        searchAdapter.setSearched(true);
+        searchAdapter.refresh(getDatas(10));
+    }
+
+    private List<Friend> getDatas(int num) {
+        List<Friend> mList = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            Friend friend = new Friend();
+            friend.setRemarkName("name" + i);
+            friend.setLabel("label" + i);
+            friend.setUserId(1000 + i);
+            mList.add(friend);
+        }
+
+        return mList;
     }
 
     @Override
@@ -83,8 +143,11 @@ public class SearchActivity extends BaseActivity
     public void onTextChanged(CharSequence s, int start, int before, int count) {
         if (TextUtils.isEmpty(s)) {
             mClear.setVisibility(View.GONE);
-        } else
+            searchHistory();
+        } else {
             mClear.setVisibility(View.VISIBLE);
+            search();
+        }
     }
 
     @Override
@@ -95,13 +158,21 @@ public class SearchActivity extends BaseActivity
     @Override
     public boolean onKey(View v, int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_ENTER) {
-            // 先隐藏键盘
-            ((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE))
-                    .hideSoftInputFromWindow(this.getCurrentFocus()
-                            .getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-            //进行搜索操作的方法，在该方法中可以加入mEditSearchUser的非空判断
+            hideSoftKeyboard();
             search();
         }
         return false;
+    }
+
+    private void hideSoftKeyboard(){
+        // 先隐藏键盘
+        ((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE))
+                .hideSoftInputFromWindow(this.getCurrentFocus()
+                        .getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    @Override
+    public void onItemClick(View view, List data, int position) {
+        ToastUtils.showShort(mContext, "postion=" + position);
     }
 }
---
+++ app/src/main/java/com/henry/hh/adapter/SearchAdapter.java
@@ -0,0 +1,114 @@
+package com.henry.hh.adapter;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.henry.hh.R;
+import com.henry.hh.entity.Friend;
+import com.henry.hh.interfaces.OnRecyclerItemClickListener;
+import com.henry.library.View.CircleImageView;
+import com.henry.library.adapter.BaseRecyclerAdapter;
+import com.henry.library.adapter.RecyclerHolder;
+
+import java.util.List;
+
+/**
+ * Date: 2016/10/27. 9:30
+ * Creator: henry
+ * Email: heneymark@gmail.com
+ * Description: 搜索历史、搜索结果的适配器
+ */
+public class SearchAdapter extends BaseRecyclerAdapter<SearchAdapter.ViewHolder, Friend>
+        implements View.OnClickListener {
+    //点击监听事件
+    private OnRecyclerItemClickListener mOnItemClickListener = null;
+    private boolean isSearched = false;
+
+    public SearchAdapter(Context context) {
+        super(context);
+    }
+
+    public SearchAdapter(Context context, List<Friend> datalist) {
+        super(context, datalist);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.item_search, parent, false);
+        //将创建的View注册点击事件
+        view.setOnClickListener(this);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
+        Friend friend = datalist.get(position);
+        //备注名称（账号）
+        holder.mRemarkName.setText(friend.getRemarkName() + "  (" + friend.getUserId() + ")");
+        //
+        if (isSearched) {
+            holder.mNickName.setVisibility(View.VISIBLE);
+            holder.mLabel.setVisibility(View.GONE);
+            holder.mNickName.setText("昵称："
+                    + (friend.getNickname() == null ? "" : friend.getNickname()));
+        } else {
+            holder.mNickName.setVisibility(View.GONE);
+            holder.mLabel.setVisibility(View.VISIBLE);
+            if (TextUtils.isEmpty(friend.getLabel()))
+                holder.mLabel.setVisibility(View.INVISIBLE);
+            holder.mLabel.setText(friend.getLabel());
+        }
+
+        holder.itemView.setTag(position);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //使用getTag方法获取数据
+        if (mOnItemClickListener != null)
+            mOnItemClickListener.onItemClick(v, datalist, (Integer) v.getTag());
+    }
+
+    /**
+     * 设置是否搜索
+     * true:显示搜索内容
+     * false:显示历史收索记录
+     *
+     * @param searched
+     */
+    public void setSearched(boolean searched) {
+        isSearched = searched;
+    }
+
+    /**
+     * item点击事件
+     *
+     * @param listener
+     */
+    public void addOnItemClickListener(OnRecyclerItemClickListener listener) {
+        this.mOnItemClickListener = listener;
+    }
+
+    class ViewHolder extends RecyclerHolder {
+        //头像
+        public CircleImageView mAvatar;
+        //备注名称
+        public TextView mRemarkName;
+        //昵称
+        public TextView mNickName;
+        //标签
+        public TextView mLabel;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mAvatar = getViewById(R.id.civ_avatar);
+            mRemarkName = getViewById(R.id.tv_name);
+            mNickName = getViewById(R.id.tv_nickname);
+            mLabel = getViewById(R.id.tv_label);
+        }
+    }
+}
