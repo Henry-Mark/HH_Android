d78b981fceb93452fc44bfff02ee6667338571da
Henry-Mark <heneymark@gmail.com>
2016-10-27 20:30:20 +0800

    IMU

--- app/src/main/java/com/henry/hh/fragment/LivingCircleLogFragment.java
+++ app/src/main/java/com/henry/hh/fragment/LivingCircleLogFragment.java
@@ -3,6 +3,7 @@ package com.henry.hh.fragment;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -21,19 +22,25 @@ import com.henry.library.fragment.BaseFragment;
 import com.henry.library.fragment.BaseLogFragment;
 import com.henry.library.utils.LogUtils;
 import com.henry.library.utils.TimeUtils;
+import com.henry.library.utils.ToastUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import cn.bingoogolapple.refreshlayout.BGANormalRefreshViewHolder;
+import cn.bingoogolapple.refreshlayout.BGARefreshLayout;
 
 /**
  * A simple {@link Fragment} subclass.
  * 生活圈，主要用于展示生活动态
  */
-public class LivingCircleLogFragment extends BaseFragment {
+public class LivingCircleLogFragment extends BaseFragment
+        implements BGARefreshLayout.BGARefreshLayoutDelegate{
 
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLayoutManager;
     private LivingCircleAdapter circleAdapter;
+    private BGARefreshLayout mRefreshLayout;
 
     public LivingCircleLogFragment() {
         // Required empty public constructor
@@ -43,6 +50,7 @@ public class LivingCircleLogFragment extends BaseFragment {
     protected void doCreateView(Bundle savedInstanceState) {
         setContentView(R.layout.fragment_living_circle);
         initList();
+        initRefresh();
         circleAdapter.refresh(getDatas(3));
 //        CircleTextImageView text = getViewById(R.id.text);
 //        TextView time = getViewById(R.id.tv_time);
@@ -56,6 +64,9 @@ public class LivingCircleLogFragment extends BaseFragment {
 //        time.setText(date2);
     }
 
+    /**
+     * 初始化列表
+     */
     private void initList(){
         mRecyclerView = getViewById(R.id.recycler_livingcircle);
         //创建默认的线性LayoutManager
@@ -66,6 +77,16 @@ public class LivingCircleLogFragment extends BaseFragment {
         //创建并设置Adapter
         circleAdapter = new LivingCircleAdapter(getActivity());
         mRecyclerView.setAdapter(circleAdapter);
+    }
+
+    /**
+     * 初始化刷新控件
+     */
+    private void initRefresh() {
+        mRefreshLayout = getViewById(R.id.refreshLayout);
+        mRefreshLayout.setDelegate(this);
+        mRefreshLayout.setIsShowLoadingMoreView(true);
+        mRefreshLayout.setRefreshViewHolder(new BGANormalRefreshViewHolder(getActivity().getApplicationContext(), true));
     }
 
     private List<LivingCircleDynamic> getDatas(int num) {
@@ -81,5 +102,29 @@ public class LivingCircleLogFragment extends BaseFragment {
         }
 
         return mList;
+    }
+
+    @Override
+    public void onBGARefreshLayoutBeginRefreshing(BGARefreshLayout refreshLayout) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mRefreshLayout.endRefreshing();
+            }
+        }, 3000);
+
+    }
+
+    @Override
+    public boolean onBGARefreshLayoutBeginLoadingMore(BGARefreshLayout refreshLayout) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                circleAdapter.refresh(getDatas(10));
+                ToastUtils.showShort(getActivity(), "没有最新数据了");
+                mRefreshLayout.endLoadingMore();
+            }
+        }, 2000);
+        return true;
     }
 }
