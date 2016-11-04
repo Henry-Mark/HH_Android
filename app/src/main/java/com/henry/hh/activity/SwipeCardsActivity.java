e00ad78175ca8740768575a1aa56b1bfd752c52a
Henry <heneymark@gmail.com>
2016-11-04 11:40:13 +0800

    IMU

--- app/src/main/java/com/henry/hh/activity/SwipeCardsActivity.java
+++ app/src/main/java/com/henry/hh/activity/SwipeCardsActivity.java
@@ -1,18 +1,119 @@
 package com.henry.hh.activity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.henry.hh.R;
 import com.henry.library.activity.TitleActivity;
+import com.henry.swipecardslibrary.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
 
 /**
  * 可滑动卡片
  */
-public class SwipeCardsActivity extends TitleActivity {
+public class SwipeCardsActivity extends TitleActivity implements View.OnClickListener {
 
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
+    private int i;
+    SwipeFlingAdapterView flingContainer;
+    private Button left,right;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_cards);
+
+        flingContainer = (SwipeFlingAdapterView)findViewById(R.id.frame);
+        left = (Button)findViewById(R.id.left);
+        right = (Button)findViewById(R.id.right);
+        left.setOnClickListener(this);
+        right.setOnClickListener(this);
+
+        al = new ArrayList<>();
+        al.add("php");
+        al.add("c");
+        al.add("python");
+        al.add("java");
+        al.add("html");
+        al.add("c++");
+        al.add("css");
+        al.add("javascript");
+
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al );
+
+
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                al.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                makeToast(mContext, "Left!");
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                makeToast(mContext, "Right!");
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+                al.add("XML ".concat(String.valueOf(i)));
+                arrayAdapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+                i++;
+            }
+
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+                View view = flingContainer.getSelectedView();
+                view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
+                view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
+            }
+        });
+
+
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                makeToast(mContext, "Clicked!");
+            }
+        });
+    }
+
+    static void makeToast(Context ctx, String s){
+        Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.left:
+                flingContainer.getTopCardListener().selectLeft();
+                break;
+            case R.id.right:
+                flingContainer.getTopCardListener().selectRight();
+                break;
+            default:
+                break;
+        }
     }
 }
+
